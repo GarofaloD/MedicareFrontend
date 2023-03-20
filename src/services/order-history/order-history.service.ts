ba28aa0885cfab0603cdc9743bf709df3a071855
danielgarofalo <garofalodaniel@gmail.com>
2023-03-20 18:09:11 -0500

    order history service added

---
+++ src/services/order-history/order-history.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {ProductCategory} from "../../common/product-category/product-category";
+import {OrderHistory} from "../../common/order-history/order-history";
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderHistoryService {
+
+  //PROPERTIES
+   private baseUrl_checkOrders = 'http://localhost:8080/api/systemOrders';
+
+   //CONSTRUCTOR
+  constructor(private httpClient: HttpClient) { }
+
+  //CUSTOM METHODS
+
+
+  //Get all the orders from the backend
+  getOrderHistory(userEmail: string):Observable<GetResponseOrderHistory>{
+
+    let orderHistoryURL = `${this.baseUrl_checkOrders}/search/findByCustomerEmail?email=${userEmail}`
+
+    return this.httpClient.get<GetResponseOrderHistory>(orderHistoryURL)
+
+  }
+
+}
+
+//Get everything from inside the JSON response (_embedded)
+interface GetResponseOrderHistory{
+
+  _embedded:{
+    systemOrders:OrderHistory[];
+  }
+}
